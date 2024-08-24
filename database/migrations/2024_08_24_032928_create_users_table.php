a1be4f8f7affc1be5f70d773e6395b8992bff536
aiyuyang <1993832332@qq.com>
2024-08-24 15:51:36 +1000

    t6.1

---
+++ database/migrations/2024_08_24_032928_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('gender');
+            $table->string('photo');
+            $table->string('university');
+            $table->string('degree');
+            $table->string('year');
+            $table->date('dob');
+            $table->string('personal_description');
+            $table->string('skills');
+            $table->string('hobbies');
+            $table->string('enrollment_type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
