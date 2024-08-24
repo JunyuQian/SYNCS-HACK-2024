443e82962f06a2e52bc6e47e491fc843d9adfd2d
--add <2813250380@qq.com>
2024-08-24 16:33:20 +1000

    add email and psw to user table

--- database/migrations/2024_08_24_032928_create_users_table.php
+++ database/migrations/2024_08_24_032928_create_users_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('email')->unique();
+            $table->string('password');
             $table->string('first_name');
             $table->string('last_name');
             $table->string('gender');
